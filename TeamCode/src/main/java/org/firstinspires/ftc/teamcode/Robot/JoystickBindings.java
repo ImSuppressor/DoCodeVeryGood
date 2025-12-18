a23ecd50f0c13f0a63b17fece4761f0fa4d93e2a
rys4590 <ronnie.yadid@gmail.com>
2025-12-18 10:37:54 +0200

    bf checkout

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/JoystickBindings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/JoystickBindings.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 public class JoystickBindings {
+    public final Gamepad gamepad1;
+    public final Gamepad gamepad2;
+
+    public JoystickBindings(Gamepad gamepad1, Gamepad gamepad2){
+        this.gamepad1=gamepad1;
+        this.gamepad2=gamepad2;
+    }
+    public void mainJoystickButtons(Robot robot){
+        if(gamepad1.x){
+
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
 public class Robot {
+    public static final Flywheel flywheel = new Flywheel()
 }
