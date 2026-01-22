92e95f09e46c5c6a713ebd22d3c7a3cd09e205d6
colin_kavanaugh <ckab3411@gmail.com>
2026-01-22 16:59:48 -0600

    Revert "Update DriveCode.java"
    This reverts commit 7990a9dbd1cb36ec8313e76e44190772b5351f1b.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EpsilonOpModes/DriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EpsilonOpModes/DriveCode.java
@@ -574,7 +574,7 @@ public class DriveCode extends OpMode {
             Boot3 = false;
         }
         //SequencedShot
-        if (gamepad1.dpad_down || SequenceShoot) {
+        if (gamepad1.dpad_down || SequenceShoot == true) {
             if (!initialized) {
                 timer2.reset();
                 SequenceShoot = true;
